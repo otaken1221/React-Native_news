ad00a53fa1bf1e93c204ff88e4300dffa94492ff
Takeru Otsu <takeru.o.2002@gmail.com>
2022-09-09 18:01:44 +0900

    edit App.js

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <View style={styles.moziBox}></View>
+        <View style={styles.gazoBox}></View>
+      </View>
     </View>
   );
 }
@@ -16,5 +18,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  box: {
+    height: 100,
+    width: "100%",
+    borderColor: "lightblue",
+    borderWidth: 1,
+    flexDirection: "row",
+  },
+
+  moziBox: {
+    flex: 1,
+    backgroundColor: "steelblue",
+    padding: 16,
+  },
+
+  gazoBox: {
+    width: 100,
+    backgroundColor: "powderblue",
   },
 });
